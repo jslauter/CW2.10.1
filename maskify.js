372cfcd9d1e8cd534451a2b06021cadae878281e
jslauter <74632945+jslauter@users.noreply.github.com>
2021-02-15 23:00:47 -0800

    Create maskify.js

---
+++ maskify.js
@@ -0,0 +1,30 @@
+// Your task is to write a function maskify,
+// which changes all but the last four characters into '#'
+
+// console.log(maskify("abcdefghijklmnop"))
+
+function maskify(cc) {
+  // If length is greater than 4, then we have things to mask
+  if (cc.length > 4) {
+      // reverse string
+      let reversed = reverse(cc);
+      let newString = '';
+      for (let i = 0; i < reversed.length; i++) {
+        // if i < 4, we want to reveal these numbers in our output
+        if (i < 4) {
+          newString += reversed[i];
+        } else {
+          // otherwise, just hide it
+          newString += '#';
+        }
+      }
+      // return the reversal of the string to revert it back to original format
+      return reverse(newString);
+  } else {
+      return cc;
+  }
+}
+
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
